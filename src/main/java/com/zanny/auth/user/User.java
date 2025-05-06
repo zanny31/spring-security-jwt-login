d1f965fc7a51a608a0c49001175cf589f029eb3e
zanny31 <jianb0331@naver.com>
2025-05-07 00:28:51 +0900

    fix(WIP) : User 클래스에 BaseTimeEntity 상속 키워드 누락 수정

--- src/main/java/com/zanny/auth/user/User.java
+++ src/main/java/com/zanny/auth/user/User.java
@@ -1,5 +1,7 @@
 package com.zanny.auth.user;
 
+import com.zanny.auth.common.BaseTimeEntity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -15,7 +17,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
+public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
