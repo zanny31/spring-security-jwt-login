7a25488e6425c7d083cb0ccc41183cc0599060ec
zanny31 <jianb0331@naver.com>
2025-05-08 13:52:00 +0900

    fix(WIP): User 엔티티의 테이블 명 변경 (user -> users)
    - H2에서 user는 예약어로 사용됨

--- src/main/java/com/zanny/auth/user/domain/User.java
+++ src/main/java/com/zanny/auth/user/domain/User.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,6 +18,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
 public class User extends BaseTimeEntity {
 
     @Id
